1971f883bfc94fbd893057a5fa92d0b1f1a8f88f
Tilman Aurich <tilman.aurich@fu-berlin.de>
2026-04-28 14:25:06 +0200

    Added basic TOPPView fixes from tempfilemanager to have FastMode work on its own

--- src/openms_gui/source/VISUAL/APPLICATIONS/TOPPViewBase.cpp
+++ src/openms_gui/source/VISUAL/APPLICATIONS/TOPPViewBase.cpp
@@ -463,10 +463,6 @@ namespace OpenMS
 
     // non-editable parameters
 
-    // not in Dialog (yet?)
-    defaults_.setValue("preferences:topp_cleanup", "true", "If the temporary files for calling of TOPP tools should be removed after the call.");
-    defaults_.setValidStrings("preferences:topp_cleanup", {"true", "false"});
-
     defaults_.setValue("preferences:version", "none", "OpenMS version, used to check if the TOPPView.ini is up-to-date");
     subsections_.emplace_back("preferences:RecentFiles");
 
@@ -1840,14 +1836,14 @@ namespace OpenMS
         auto l = getCurrentLayer();
         if (l)
         {
-          auto annotator = LayerAnnotatorBase::getAnnotatorWhichSupports(topp_.file_name + "_in");
+          auto annotator = LayerAnnotatorBase::getAnnotatorWhichSupports(topp_.file_name_in);
           if (annotator.get() == nullptr)
           { // no suitable annotator? open new layer/window
-            addDataFile(topp_.file_name + "_out", true, false, topp_.layer_name + " (" + topp_.tool + ")", topp_.window_id, topp_.spectrum_id);
+            addDataFile(topp_.file_name_out, true, false, topp_.layer_name + " (" + topp_.tool + ")", topp_.window_id, topp_.spectrum_id);
           }
           else
           { // we have an annotator ... let's annotate the current layer
-            annotator->annotateWithFilename(*l, *log_, topp_.out + "_out"); // ID tabs are automatically enabled
+            annotator->annotateWithFilename(*l, *log_, topp_.file_name_out); // ID tabs are automatically enabled
           }
         }
       }
@@ -1858,13 +1854,6 @@ namespace OpenMS
     topp_.process = nullptr;
     updateMenu();
 
-    // clean up temporary files
-    if (param_.getValue("preferences:topp_cleanup") == "true")
-    {
-      File::remove(topp_.file_name + "_ini");
-      File::remove(topp_.file_name_in);
-      File::remove(topp_.file_name_out);
-    }
   }
 
   const LayerDataBase* TOPPViewBase::getCurrentLayer() const
@@ -2032,7 +2021,7 @@ namespace OpenMS
     const LayerDataBase& layer = getActiveCanvas()->getCurrentLayer();
     
     ExperimentSharedPtrType exp = std::make_shared<AnnotatedMSRun>();
-    exp.get()->getMSExperiment() = std::move(IMDataConverter::reshapeIMFrameToMany(spec));
+    exp.get()->getMSExperiment() = IMDataConverter::reshapeIMFrameToMany(spec);
     // hack, but currently not avoidable, because 2D widget does not support IM natively yet...
     // for (auto& spec : exp->getSpectra()) spec.setRT(spec.getDriftTime());
 
